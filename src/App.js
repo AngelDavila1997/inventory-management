97fca577f567844efde11650f840f19657dd06bc
Roberto Rivera <a07025304@itesm.mx>
2020-05-11 23:33:16 -0500

    added test back and fixed packages

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AddArticle from './components/AddArticle';
 import AvailableArticles from './components/AvailableArticles';
 import AvailableProviders from './components/AvailableProviders';
 import UserList from './components/UserList';
+import Test from './Test'
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import './App.css';
@@ -43,6 +44,7 @@ class App extends Component {
           <Route path="/movementlist" component={Dashboard}/>
           <Route path="/addnewuser" component={NuevoUsuario}/>
           <Route path="/userlist" component={UserList}/>
+          <Route path="/test" component={Test}/>
         </Switch>
       </div>
     </Router>
